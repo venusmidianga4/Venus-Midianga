f9e58ffb5edf352e5e1bcc3ceb94886a550c785b
venusmidianga4 <aom39@scarletmail.rutgers.edu>
2026-04-13 14:20:43 -0400

    Update script.js

--- jstest/js/script.js
+++ jstest/js/script.js
@@ -1,9 +1,19 @@
-let title = document.getElementById("title");
+// Variables
+let message = document.getElementById("message");
 let button = document.getElementById("btn");
 
-function changeContent() {
-    title.textContent = "It worked!";
-    title.style.color = "red";
+// Function
+function changeMessage() {
+    message.textContent = "You clicked the button!";
+    message.style.color = "green";
 }
 
-button.addEventListener("click", changeContent);
+// Another function (for key press)
+function keyPressed(event) {
+    message.textContent = "You pressed: " + event.key;
+    message.style.color = "blue";
+}
+
+// Event Listeners
+button.addEventListener("click", changeMessage);
+document.addEventListener("keydown", keyPressed);
